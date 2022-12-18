d38a2ba245359505af41c61bd4f75f4df32775b6
Dawid Rzeszótko <dawid.rzeszotko@nekken.pl>
2022-12-18 14:24:55 +0100

    messaproducer

--- src/main/java/com/example/rabbitmqproject/rabbitmq/core/MessageProducer.java
+++
@@ -1,7 +0,0 @@
-package com.example.rabbitmqproject.rabbitmq.core;
-
-public interface MessageProducer {
-
-  void send(String exchange, String routingKey, String message);
-
-}
--- src/main/java/com/example/rabbitmqproject/rabbitmq/core/MessageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.rabbitmqproject.rabbitmq.core;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface MessageRepository extends JpaRepository<MessageEntity, Long> {
-
-}
